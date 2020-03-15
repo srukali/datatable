d4a77f1f2b7b4bc0f7454705b689995d2d64f624
Katelyn Smith <katelyn@agentology.com>
2020-03-14 18:45:41 -0700

    drag and drop headers implemented... columns not quite working yet

--- datatable/src/app/datatable/datatable.component.ts
+++ datatable/src/app/datatable/datatable.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { CdkDragStart, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort} from '@angular/material/sort';
 
 import { ApiService } from '../shared/api.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
 @Component({
   selector    : 'app-datatable',
@@ -20,27 +14,48 @@ export interface PeriodicElement {
 export class DatatableComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
-  displayedColumns: string[] = ['id', 'employee_name', 'employee_salary', 'employee_age'];
+  // I wouldn't typically name variables in snake case, this is how my mock API sends them :)
+  columns: any[] = [
+    { field: 'id' },
+    { field: 'employee_name' },
+    { field: 'employee_salary' },
+    { field: 'employee_age' }
+  ];
+  displayedColumns: string[] = [];
   dataSource = new MatTableDataSource();
-
+  previousIndex: number;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
+    this.setDisplayedColumns();
     this.dataSource.sort = this.sort;
-    console.log('this.dataSource.sort ', this.dataSource.sort );
 
     this.apiService.getEmployees()
       .subscribe((data) => {
-        console.log('data', data);
         this.dataSource.data = data.data;
-        console.log('this.dataSource.data', this.dataSource.data);
-
       });
-
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  setDisplayedColumns() {
+    this.columns.forEach(( column, index) => {
+      column.index = index;
+      this.displayedColumns[index] = column.field;
+    });
+  }
+
+  dragStarted(event: CdkDragStart, index: number ) {
+    this.previousIndex = index;
+  }
+
+  dropListDropped(event: CdkDropList, index: number) {
+    if (event) {
+      moveItemInArray(this.columns, this.previousIndex, index);
+      this.setDisplayedColumns();
+    }
   }
 }
--- datatable/src/app/datatable/datatable.module.ts
+++ datatable/src/app/datatable/datatable.module.ts
@@ -4,6 +4,13 @@ import { DatatableComponent} from './datatable.component';
 import { MatTableModule} from '@angular/material/table';
 import { MatInputModule} from '@angular/material/input';
 import { MatSortModule } from '@angular/material/sort';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule} from '@angular/common';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {A11yModule} from '@angular/cdk/a11y';
+
 
 @NgModule({
   declarations: [
@@ -13,14 +20,26 @@ import { MatSortModule } from '@angular/material/sort';
     MatFormFieldModule,
     MatTableModule,
     MatInputModule,
-    MatSortModule
+    MatSortModule,
+    DragDropModule,
+    CommonModule,
+    ScrollingModule,
+    CdkTableModule,
+    CdkTreeModule,
+    A11yModule
   ],
   exports: [
     DatatableComponent,
     MatFormFieldModule,
     MatTableModule,
     MatInputModule,
-    MatSortModule
+    MatSortModule,
+    DragDropModule,
+    CommonModule,
+    ScrollingModule,
+    CdkTableModule,
+    CdkTreeModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: []
